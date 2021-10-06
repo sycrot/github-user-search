45736efe44fc21ee907a9770456655e2f62dc590
Thiago Farias <thiagofarias99@hotmail.com>
2021-10-06 15:52:58 -0300

    apiget 1552061021

---
+++ assets/js/apiController.js
@@ -0,0 +1,109 @@
+const avatar = document.querySelector('.peopleinformation--image img')
+const identityName = document.querySelector('.identity--name')
+const identityUsername = document.querySelector('.identity--username')
+const identityJoineddate = document.querySelector('.identity--joineddate')
+const bio = document.querySelector('.bio--pbio')
+const numbersNumrepos = document.querySelector('#numbers-numrepos')
+const numbersNumfollowers = document.querySelector('#numbers-numfollowers')
+const numbersNumfollowing = document.querySelector('#numbers-numfollowing')
+
+const listitemLocation = document.querySelector('#listitem-location')
+const listitemBlog = document.querySelector('#listitem-blog')
+const listitemTwitter = document.querySelector('#listitem-twitter')
+const listitemUser = document.querySelector('#listitem-user')
+
+
+
+function getData(user) {
+    fetch(`https://api.github.com/users/${user}`)
+    .then(res => res.json())
+    .then(jsonData => {
+        let data = {}
+        data = {
+            avatar_url : jsonData.avatar_url,
+            name: jsonData.name,
+            bio : jsonData.bio,
+            blog : jsonData.blog,
+            created_at : jsonData.created_at,
+            email : jsonData.email,
+            followers : jsonData.followers,
+            followers_url : jsonData.followers_url,
+            following : jsonData.following,
+            html_url : jsonData.html_url,
+            url : jsonData.url,
+            location : jsonData.location,
+            login : jsonData.login,
+            public_gists : jsonData.public_gists,
+            public_repos : jsonData.public_repos,
+            repos_url : jsonData.repos_url,
+            updated_at : jsonData.updated_at,
+            twitter_username : jsonData.twitter_username
+        }
+        let date = new Date(jsonData.created_at)
+        date = dateFormat(date)
+
+        avatar.setAttribute('src', data.avatar_url)
+        identityName.innerHTML = data.name
+        identityUsername.innerHTML = `@${data.login}`
+        identityJoineddate.innerHTML = date
+        bio.innerHTML = data.bio
+        numbersNumrepos.innerHTML = data.public_repos
+        numbersNumfollowers.innerHTML = data.followers
+        numbersNumfollowing.innerHTML = data.following
+
+        if (data.location == null) {
+            listitemLocation.classList.add('listitem-notavailable')
+            listitemLocation.innerHTML = 'Not Available'
+        } else {
+            listitemLocation.innerHTML = data.location
+        }
+
+        if (data.twitter_username == null) {
+            listitemTwitter.classList.add('listitem-notavailable')
+            listitemTwitter.innerHTML = 'Not Available'
+        } else {
+            listitemTwitter.innerHTML = data.twitter_username
+        }
+
+        if (data.blog === "") {
+            listitemBlog.classList.add('listitem-notavailable')
+            listitemBlog.innerHTML = 'Not Available'
+        } else {
+            listitemBlog.innerHTML = data.blog
+        }
+
+        listitemUser.innerHTML = `@${data.login}`
+        listitemUser.setAttribute('href', data.html_url)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+inputEntry()
+function inputEntry() {
+    const input = document.querySelector('.search--input')
+    
+    input.addEventListener('keyup', e => {
+        if (e.key === 'Enter') {
+            getData(input.value)
+        }
+    })
+}
+
+function dateFormat(date) {
+    let months = ["Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"]
+    let dateFormat = 'Entrou em ' + ((date.getDate())) + " de " + months[(date.getMonth())] + ' de ' + date.getFullYear()
+    return dateFormat
+}
